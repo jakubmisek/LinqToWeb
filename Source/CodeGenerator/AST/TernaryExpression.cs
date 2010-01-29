4c6d8a8e7cd26fff0ce60151d18c473ced1896dc
Jakub Míšek <jakubmisek@users.noreply.github.com>
2010-01-29 23:28:30 +0000

    some missing files

---
+++ Source/CodeGenerator/AST/TernaryExpression.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linqtoweb.CodeGenerator.AST
+{
+    /*public class IfExpression:Expression
+    {
+
+    }*/
+
+    /// <summary>
+    /// expr ? expr : expr
+    /// </summary>
+    public class TernaryCondExpression : Expression
+    {
+        public readonly Expression ConditionExpr, Expr1, Expr2;
+
+        public TernaryCondExpression(ExprPosition position, Expression condition, Expression expr1, Expression expr2)
+            :base(position)
+        {
+            this.ConditionExpr = condition;
+            this.Expr1 = expr1;
+            this.Expr2 = expr2;
+        }
+    }
+}
---
+++ Source/CodeGenerator/AST/UnaryExpression.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace linqtoweb.CodeGenerator.AST
+{
+    public class UnaryExpression : Expression
+    {
+        /// <summary>
+        /// the value of the unary expression.
+        /// </summary>
+        public readonly Expression Value;
+
+        /// <summary>
+        /// Unary expression initialization.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="value"></param>
+        public UnaryExpression(ExprPosition position, Expression value)
+            :base(position)
+        {
+            this.Value = value;
+        }
+    }
+
+    /// <summary>
+    /// !value
+    /// </summary>
+    public class LogicalNotExpression : UnaryExpression
+    {
+        public LogicalNotExpression(ExprPosition position, Expression value)
+            :base(position, value)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// -value
+    /// </summary>
+    public class UnaryMinusExpression : UnaryExpression
+    {
+        public UnaryMinusExpression(ExprPosition position, Expression value)
+            : base(position, value)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// +value
+    /// </summary>
+    public class UnaryPlusExpression : UnaryExpression
+    {
+        public UnaryPlusExpression(ExprPosition position, Expression value)
+            : base(position, value)
+        {
+
+        }
+    }
+}
