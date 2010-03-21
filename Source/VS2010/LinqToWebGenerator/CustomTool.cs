4a0a4eafbfba054994a9a67faa0674ede69e4339
Jakub Míšek <jakubmisek@users.noreply.github.com>
2010-03-21 03:41:17 +0000

    setup for VS2010, including Custom Tool for generating .cs

---
+++ Source/VS2010/LinqToWebGenerator/CustomTool.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell.Interop;
+
+using linqtoweb.CodeGenerator;
+using linqtoweb.CodeGenerator.AST;
+
+namespace LinqToWebGenerator
+{
+    [Guid("55CA59B6-C689-4FAC-B494-5368229E7E2B")]
+    [ComVisible(true)]
+    public class CustomTool : Microsoft.VisualStudio.TextTemplating.VSHost.BaseCodeGeneratorWithSite
+    {
+        public override string GetDefaultExtension()
+        {
+            return ".design.cs";
+        }
+
+        protected override byte[] GenerateCode(string inputFileName, string inputFileContent)
+        {
+            string code = string.Empty;
+
+            Scanner scanner = new Scanner();
+            scanner.SetSource(inputFileContent, 0);
+
+            Parser parser = new Parser(scanner);
+            if (parser.Parse())
+            {
+                GlobalCode x = parser.Ast;
+
+                var ms = new MemoryStream();
+                var sw = new StreamWriter(ms, Encoding.Unicode);
+
+                x.EmitCs(sw, FileNamespace, Path.GetFileNameWithoutExtension(inputFileName));
+
+                ms.Position = 0;
+                code = new StreamReader(ms).ReadToEnd();
+            }
+            else
+            {
+                //
+            }
+
+            return Encoding.UTF8.GetBytes(code);
+        }
+    }
+
+}
---
+++ Source/VS2010/LinqToWebGenerator/InstallerHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+
+namespace YUICustomTool
+{
+    [RunInstaller(true)]
+    public partial class InstallerHelper : Installer
+    {
+        public InstallerHelper()
+        {
+            //InitializeComponent();
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(System.Collections.IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            // Get the location of regasm
+            string regasmPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory() + @"regasm.exe";
+            // Get the location of our DLL
+            string componentPath = typeof(InstallerHelper).Assembly.Location;
+            // Execute regasm
+            System.Diagnostics.Process.Start(regasmPath, "/codebase \"" + componentPath + "\"");
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+    }
+}
